541a4b33bc32283c1507f8d8065ee0c63a608c1c
weleyam <weleyam@gmail.com>
2017-07-20 11:03:45 +0700

    Add files via upload

---
+++ ApplikasiShutdown/ApplikasiShutdown/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace ApplikasiShutdown
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timerWaktu = new System.Windows.Forms.NumericUpDown();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.btnPause = new System.Windows.Forms.Button();
+            this.btnResume = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.timerWaktu)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timerWaktu
+            // 
+            this.timerWaktu.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.timerWaktu.Location = new System.Drawing.Point(189, 20);
+            this.timerWaktu.Name = "timerWaktu";
+            this.timerWaktu.Size = new System.Drawing.Size(192, 62);
+            this.timerWaktu.TabIndex = 0;
+            this.timerWaktu.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(189, 94);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(89, 35);
+            this.btnStart.TabIndex = 4;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStop.Location = new System.Drawing.Point(292, 94);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(89, 35);
+            this.btnStop.TabIndex = 5;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(85, 19);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(68, 35);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(8, 64);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(68, 35);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "1,5";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(85, 64);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(68, 35);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "2";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // btnPause
+            // 
+            this.btnPause.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPause.Location = new System.Drawing.Point(189, 94);
+            this.btnPause.Name = "btnPause";
+            this.btnPause.Size = new System.Drawing.Size(89, 35);
+            this.btnPause.TabIndex = 9;
+            this.btnPause.Text = "Pause";
+            this.btnPause.UseVisualStyleBackColor = true;
+            this.btnPause.Click += new System.EventHandler(this.btnPause_Click);
+            // 
+            // btnResume
+            // 
+            this.btnResume.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnResume.Location = new System.Drawing.Point(189, 94);
+            this.btnResume.Name = "btnResume";
+            this.btnResume.Size = new System.Drawing.Size(89, 35);
+            this.btnResume.TabIndex = 10;
+            this.btnResume.Text = "Resume";
+            this.btnResume.UseVisualStyleBackColor = true;
+            this.btnResume.Click += new System.EventHandler(this.btnResume_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(8, 19);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(68, 35);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "1/2";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button4);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Location = new System.Drawing.Point(12, 14);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(161, 114);
+            this.groupBox1.TabIndex = 12;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Waktu";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(398, 148);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnResume);
+            this.Controls.Add(this.btnPause);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.timerWaktu);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Timer Shutdown (Created by Weleyam)";
+            this.Activated += new System.EventHandler(this.Form1_Activated);
+            this.Deactivate += new System.EventHandler(this.Form1_Deactivate);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.timerWaktu)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.NumericUpDown timerWaktu;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button btnPause;
+        private System.Windows.Forms.Button btnResume;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ ApplikasiShutdown/ApplikasiShutdown/Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ApplikasiShutdown
+{
+    public partial class Form1 : Form
+    {
+        int sisa;
+        public Form1()
+        {
+            InitializeComponent();
+            timerWaktu.Maximum = 10000;
+            timerWaktu.Minimum = 0;
+            aturBtn(true);
+        }
+
+        void aturBtn(bool st)
+        {
+            button1.Enabled = st;
+            button2.Enabled = st;
+            button3.Enabled = st;
+            button4.Enabled = st;
+            btnStart.Visible = st;
+            btnStop.Enabled = !st;
+            btnPause.Visible = !st;
+            btnResume.Visible = !st;
+            timerWaktu.Enabled = st;
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            aturBtn(true);
+            timerWaktu.Value = 0;
+            timer1.Stop();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timerWaktu.Value = 0;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timerWaktu.Value = 3600;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timerWaktu.Value = 5400;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            timerWaktu.Value = 7200;
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (timerWaktu.Value == 0)
+            {
+                MessageBox.Show("Nilai waktu tidak boleh 0!!", "Peringatan");
+            }
+            else
+            {
+                aturBtn(false);
+                btnResume.Visible = false;
+                timer1.Start();
+
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            sisa = (int)timerWaktu.Value; 
+            if(sisa > 0)
+            {
+                sisa--;
+                timerWaktu.Value = sisa;
+            }
+            else if(sisa == 0)
+            {
+                System.Diagnostics.Process.Start("shutdown.exe", "-s -t 0 -f");
+                timer1.Stop();
+                aturBtn(true);
+            }
+        }
+
+        private void btnPause_Click(object sender, EventArgs e)
+        {
+            aturBtn(false);
+            btnPause.Visible = false;
+            btnResume.Visible = true;
+            btnStop.Enabled = true;
+            timerWaktu.Value = sisa;
+            timer1.Stop();
+        }
+
+        private void btnResume_Click(object sender, EventArgs e)
+        {
+            aturBtn(false);
+            btnResume.Visible = false;
+            btnPause.Visible = true;
+            timer1.Start();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            timerWaktu.Value = 1800;
+        }
+
+        private void Form1_Activated(object sender, EventArgs e)
+        {
+            this.Opacity = 1.00;
+        }
+
+        private void Form1_Deactivate(object sender, EventArgs e)
+        {
+            try
+            {
+                this.Opacity = 0.25;
+            }
+            catch
+            {
+
+            }
+        }
+    }
+}
